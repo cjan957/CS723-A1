0d5c59ce2482ebe24aa85962a90e6d389a7736af
Josh Asi <jasi433@UoA.auckland.ac.nz>
2018-04-06 15:33:17 +1200

    forgot a file

---
+++ freq_relay.h
@@ -0,0 +1,25 @@
+/*
+ * freq_relay.h
+ *
+ *  Created on: 6/04/2018
+ *      Author: jasi433
+ */
+
+#ifndef FREQ_RELAY_H_
+#define FREQ_RELAY_H_
+
+// Scheduler includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+
+#include <altera_avalon_pio_regs.h>
+
+QueueHandle_t xFreqQueue;
+
+void freq_relay();
+
+
+
+#endif /* FREQ_RELAY_H_ */
